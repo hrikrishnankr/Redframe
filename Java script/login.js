abdc228c7db84d0411e440772e274b83142d0dd6
Hari Krishnan <hari@qburst.com>
2015-12-16 11:58:58 +0530

    New changes

--- Java script/login.js
+++ Java script/login.js
@@ -1,10 +1,15 @@
 function loginValidate(){
 	var uname,pass,flash;
-	flash=document.getElementById("flash")
+	flash_uname=document.getElementById("flash_uname");
+	flash_pass=document.getElementById("flash_pass");
 	uname=document.getElementById("uname");
 	pass=document.getElementById("pass");
-	if(uname.value=="")
-		flash.innerHTML="Username cannot be null";
-	else if(pass.value=="")
-		flash.innerHTML="Password cannot be null";
+	if(uname.value==""){
+		flash_uname.innerHTML="Username cannot be null";
+		return false;
+	}
+	else if(pass.value==""){
+		flash_pass.innerHTML="Password cannot be null";
+		return false;
+	}
 }
--- Java script/register.js
+++ Java script/register.js
@@ -6,29 +6,53 @@ function validate(){
 	email=document.getElementById("email");
 	uname=document.getElementById("uname");
 	pass=document.getElementById("pass");
-	flash=document.getElementById("flash");
-	output="hello "+fname+" "+lname+" you have submitted the data";
-	if(fname.value=="")
-		flash.innerHTML="First name cannnot be null";
-	else if(! (/^[a-zA-Z]+$/).test(fname.value))
-		flash.innerHTML="First name should only contain characters";
-	else if(lname.value=="")
-		flash.innerHTML="Last name cannnot be null";
-	else if(! (/^[a-zA-Z]+$/).test(lname.value))
-		flash.innerHTML="Last name should only contain characters";
-	else if(uname.value=="")
-		flash.innerHTML="Username cannnot be null";
-	else if(pass.value=="")
-		flash.innerHTML="Password cannnot be null";
-	else if(pass.value.length<7)
-		flash.innerHTML="Password length conatain least 7 letters";
-	else if(email.value=="")
-		flash.innerHTML="Email cannnot be null";
-	else if(!regx.test(email.value))
-		flash.innerHTML="Enter a valid email";
+	flash_fname=document.getElementById("flash_fname");
+	flash_lname=document.getElementById("flash_lname");
+	flash_uname=document.getElementById("flash_uname");
+	flash_pass=document.getElementById("flash_pass");
+	flash_gender=document.getElementById("flash_gender");
+	flash_email=document.getElementById("flash_email");
+	succ=document.getElementById("succ");
+	if(fname.value==""){
+		flash_fname.innerHTML="First name cannnot be null";
+		return false;
+	}
+	else if(! (/^[a-zA-Z]+$/).test(fname.value)){
+		flash_fname.innerHTML="First name should only contain characters";
+		return false;
+	}
+	else if(lname.value==""){
+		flash_lname.innerHTML="Last name cannnot be null";
+		return false;
+	}
+	else if(! (/^[a-zA-Z]+$/).test(lname.value)){
+		flash_lname.innerHTML="Last name should only contain characters";
+		return false;
+	}
+	else if(uname.value==""){
+		flash_uname.innerHTML="Username cannnot be null";
+		return false;
+	}
+	else if(pass.value==""){
+		flash_pass.innerHTML="Password cannnot be null";
+		return false;
+	}
+	else if(pass.value.length<7){
+		flash_pass.innerHTML="Password length conatain least 7 letters";
+		return false;
+	}
+	else if(email.value==""){
+		flash_email.innerHTML="Email cannnot be null";
+		return false;
+	}
+	else if(!regx.test(email.value)){
+		flash_email.innerHTML="Enter a valid email";
+		return false;
+	}
 	else{
-		flash.innerHTML="You have registered successfully";
+		succ.innerHTML="You have registered successfully";
 		alert("You have registered successfully");
+		return true;
 	}
 }
 
